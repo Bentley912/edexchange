02d16d71096c3184cbe90c040980b8812c56fd9c
Patrick Bentley <pbentley07@gmail.com>
2017-06-25 17:03:54 -0400

    Dev Environment up React Routes not functional

---
+++ app/components/Main.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Main extends Component {
+render(){
+    return(
+        <h1>Rendering Shit </h1>
+    );
+}
+
+}
+
+export default Main;
