27fd8d761be792e46d9334ee87884e45e719d819
Patrick Bentley <pbentley07@gmail.com>
2017-06-25 21:19:59 -0400

    Changes to styling

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var path = require("path");
 
-var apiRoutes = require("./apiRoutes");
 
+// var apiRoutes = require("./apiRoutes");
 var router = new express.Router();
 
 // Use the apiRoutes module for any routes starting with "/api"
-router.use("/api", apiRoutes);
+// router.use("/api", apiRoutes);
 
 // Otherwise send all other requests the index.html page
 // React router will handle routing withing the app
