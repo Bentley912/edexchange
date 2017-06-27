cc1920ac79d925f3a2dbe60507814e3facd2b60f
Patrick Bentley <pbentley07@gmail.com>
2017-06-27 15:33:09 -0400

    Added SQL and SQLize, Educators Model Created

---
+++ models/educator.js
@@ -0,0 +1,25 @@
+module.exports = function(sequelize, DataTypes){
+    var Educator = sequelize.define("Educator", {
+        firstname: {
+            type:DataTypes.STRING,
+            allowNull:false
+        },
+        lastname:{
+        type:DataTypes.STRING,
+        allowNull:false
+        },
+        email: {
+            type:DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len:[1,25],
+                isEmail:true
+            }
+        },
+        school: {
+            type:DataTypes.STRING,
+            allowNull:false
+        }
+    })
+    return Educator;
+}
