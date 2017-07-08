f24d8a1e2b4fb4823747f28d7dc6d67d03e5982d
Patrick Bentley <pbentley07@gmail.com>
2017-07-08 11:09:09 -0400

    Login Working mocing to Profile Creation

---
+++ app/components/edProfile.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class EdProfile extends Component {
+    render(){
+        return(
+            <h1> This is the Educator Profile</h1>
+        )
+    }
+}
+
+export default EdProfile;
--- app/config/routes.js
+++ app/config/routes.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Route, IndexRoute, Router, browserHistory } from "react-router";
-import Main from "../components/Main";
-import Home from "../components/Home"
-import Signup from "../components/Signup"
-import Login from "../components/Login"
-import Member from "../components/Member"
-import Option from "../components/Option"
 
+import EdProfile from "../components/EdProfile";
+import Main from "../components/Main"
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-        <IndexRoute component={Home} />
-       <Route path="signup" component={Signup} />
-       <Route path="login" component={Login} />
-       <Route path="member" component={Member} />
-       <Route path="option" component={Option} />
+    <Route path="/" component={}>
+      <Route path="edProfile" component={EdProfile} />
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 );
